8b7591089ff23e6113dc722feba47dd70a7b6c33
Simon Bergot <sbergot@cegid.com>
2026-06-16 17:40:17 +0200

    update

--- src/types/mac.ts
+++ src/types/mac.ts
@@ -1,24 +1,47 @@
 type MacClass = 1 | 2 | 3;
 
 type Score = 1 | 2 | 3 | 4 | 5 | 6;
+
 type Score0 = 0 | Score;
 
 type ModuleState = "normal" | "damaged" | "destroyed";
 
-interface MacSlot {
-  module: string;
+interface Hardware {
+  type: "hardware";
+  name: string;
+}
+
+interface Weapon {
+  type: "weapon";
+  name: string;
+}
+
+interface Brawl {
+  type: "brawl";
+  name: string;
+}
+
+type Module = Hardware | Weapon | Brawl;
+
+interface Slot {
+  module: Module;
   state: ModuleState;
 }
 
 export interface Mac {
   name: string;
   class: MacClass;
-  slots: Record<Score, MacSlot>;
+  slots: Record<Score, Slot>;
   heat: Score0;
   motion: Score;
   jolt: Score0;
   rad: Score0;
-  internalDamage: Score0;
+  internalDamage: number;
   crashed: boolean;
 }
 
+export interface MacDesign {
+  name: string;
+  class: MacClass;
+  slots: Record<Score, Module>;
+}
